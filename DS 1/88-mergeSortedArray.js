51083e33835f9b9ae0e088708e3416cbd3030e01
Eduardo Goes <e.goes@unifesp.br>
2022-12-11 07:17:09 -0300

    Solve sorted array merge problem

--- DS 1/88-mergeSortedArray.js
+++ DS 1/88-mergeSortedArray.js
@@ -6,11 +6,21 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function(nums1, m, nums2, n) {
-    let swap;
-    for (let i = 0; i < m+n; i++) {
-        if (nums2[i] < nums1[i]) {
-            // Swap nums2 with nums1
-            nums2[i] = nums1[i];
-        }
+    let size = m+n;
+    let i = m, j = 0, k = 0;
+
+    // Inserindo os elementos de nums2 ao final de nums1.
+    while (i < size) {
+        nums1[i] = nums2[j++];
+        i++;
     }
+
+    nums1.sort((a,b) => {
+        if(a > b) return 1;
+        if(a < b) return -1;
+        return 0;
+    });
 };
+
+let nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3;
+merge(nums1, m, nums2, n);
