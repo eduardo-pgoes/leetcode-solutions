f2c4e243c48d6d2555d7e88bcfef358f788c61fc
Eduardo Goes <eduardo.paulog@gmail.com>
2022-12-19 09:19:52 -0300

    Alternative solution to 1389 (target array problem)

---
+++ Arrays/1389-targetArray-alternativeSolution.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @param {number[]} index
+ * @return {number[]}
+ */
+var createTargetArray = function(nums, index) {
+    let n = index.length;
+    let target = Array(n).fill(0);
+    for (let i = 0; i < n; i++) {
+        insertAtIndex(target, nums[i], index[i]);
+    }  
+    return target;
+};
+
+function insertAtIndex(array, value, index) {
+    for (let i = array.length; i < index; i++) {
+        array[i] = array[i-1];
+    }
+    array[index] = value;
+}
+
+let nums = [0,1,2,3,4], index = [0,1,2,2,1];
+console.log(createTargetArray(nums, index));
